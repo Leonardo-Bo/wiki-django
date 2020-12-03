8af1554ccaddcf37b9f1e74bebf775446aae363d
leonardo <boleo88@gmail.com>
2020-12-03 23:43:18 +0100

    Ho impostato il reset della password. Resta da nascondere l'email in 'settings.py'

--- wiki/apps/users/urls.py
+++ wiki/apps/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from .views import UserRegisterView, RequestPage, profile, change_password
-
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('users/register/', UserRegisterView.as_view(), name='register'), 
@@ -8,4 +8,8 @@ urlpatterns = [
     path('users/', include('django.contrib.auth.urls')), 
     path('users/profile/', profile, name='profile'),
     path('users/update-password/', change_password, name='change_password'), 
+    path('users/password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
+    path('users/password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('users/password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
 ]
